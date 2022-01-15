05c0b9f5d37321c7138aa31de21e30e706ed73d7
Alifer Da Silva <aliferdasilva91@gmail.com>
2022-01-15 13:46:28 -0300

    pequena correção exerc contratos

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -10,17 +10,17 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	
+
 	private Department department;
 	//para registrar varios objetos do mesmo tipo faça uma lista e não inclua no contrutor
 	private List<HourContract> contracts= new ArrayList<>();
-	
+
 	public Worker() {
-		
+
 	}
 
 	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
-		
+
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
@@ -63,7 +63,7 @@ public class Worker {
 		return contracts;
 	}
 
-	
+
 	public void addContract(HourContract contract) {
 		contracts.add(contract);
 	}
@@ -77,9 +77,9 @@ public class Worker {
 			cal.setTime(c.getDate());
 			int c_year= cal.get(Calendar.YEAR);
 			int c_month=1+cal.get(Calendar.MONTH);
-			if() {
-			sum+=c.totalValue();
-			}
+			if(year==c_year && month==c_month) {
+				sum+=c.totalValue();
+			}	
 		}
 		return sum;
 	}
