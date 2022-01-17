2c90a435544a2d3028246c9f7aeffbc72f2e58da
Alifer Da Silva <aliferdasilva91@gmail.com>
2022-01-17 11:55:24 -0300

    Finalizando exercicio

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,9 +1,68 @@
 package apllication;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc =new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		System.out.print("Enter department's name:");
+		String departmentName =sc.nextLine();
+		System.out.println("Enter worker data: ");
+		System.out.print("Name: ");
+		String workerName=sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel=sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary= sc.nextDouble();
+		
+		Worker worker=new Worker(workerName, WorkerLevel.valueOf(workerLevel),baseSalary, new Department(departmentName));
+		//definindo numero de contratos e criando-os
+		System.out.print("How many contracts to this worker? ");
+		int n =sc.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			System.out.println("Enter contract #"+i+" data");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date contractDate =sdf.parse(sc.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour =sc.nextDouble();
+			System.out.println("Duration (hours)");
+			int hours =sc.nextInt();
+			HourContract contract=new HourContract(contractDate, valuePerHour, hours);
+			//associando o contract ao worker
+			worker.addContract(contract);
+		}
+		/*
+		 * Integer.parseInt()-> serve para transformar a string em integer
+		 * 
+		 * nomeDaVariavel.substring(0,2)  -> significa que a string será 
+		 * dividida em uma substring e guardada na variavel do comando 
+		 * e os numero indicam a posição de onde será cortada (no caso
+		 * da posição zero ao 2, se apenas colocar 3 serão selecionadas do 3 em diante*/
+		System.out.println();
+		
+		System.out.print("Enter mounth and year to calculate income (MM/YYYY): ");
+		String monthAndYear =sc.next();
+		
+		int month =Integer.parseInt(monthAndYear.substring(0, 2));
+		int year =Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: "+worker.getName());
+		System.out.println("Department: "+worker.getDepartment().getName());
+		System.out.println("Income for "+monthAndYear+": "+String.format("%.2f",worker.income(year, month)));
+		sc.close();
 
 	}
 
